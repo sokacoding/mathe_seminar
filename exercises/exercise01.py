0137cf20b653a89674de6530ea2a2ac85b8885c4
sokacoding <sokacoding@gmail.com>
2025-06-27 14:24:07 +0200

    added exercise01, test, and run.sh,

---
+++ exercises/exercise01.py
@@ -0,0 +1,17 @@
+# Aufgabe 1:
+# Implementiere eine Funktion addiere(a, b),
+# die zwei Zahlen addiert und das Ergebnis zurückgibt.
+
+# Aufgabe 2:
+# Implementiere eine Funktion multipliziere(a, b),
+# die zwei Zahlen multipliziert und das Ergebnis zurückgibt.
+
+
+def addiere(a, b):
+    # TODO: implementieren
+    return None
+
+
+def multipliziere(a, b):
+    # TODO: implementieren
+    return None
---
+++ tests/test_exercise01.py
@@ -0,0 +1,7 @@
+from exercises.exercise01 import addiere, multipliziere
+
+def test_addiere():
+    assert addiere(2, 3) == 5
+
+def test_multipliziere():
+    assert multipliziere(2, 3) == 6
